67a5db09983cf941cb212d0ef005055ed611c87a
farrel irawan <irawanssfarrel@gmail.com>
2025-02-19 23:57:07 +0700

    membuat fungsi renderBaseNotes() untuk merender notes bawaan

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,21 @@
 import "./Utils/CustomComponents.js";
 import notesData from "./data/notesData.js";
+
+const notesContainer = document.getElementById('notes-container');
+
+function renderBaseNotes(container) {
+    notesData.forEach(note => {
+        container.insertAdjacentHTML('beforeend', `
+            <div class="card-list-notes" id="${note.id}">
+                <div class="card-header">
+                    <h3>${note.title}</h3>
+                    <p>2 hari yang lalu</p>
+                </div>
+                <hr>
+                <div class="card-content">
+                    <p>${note.body}</p>
+                </div>
+            </div>
+        `);
+    });
+}
